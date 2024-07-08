39d33cef101d7c337031c57b4f99b57c71d6c028
aries20 <ariespujie12345@gmail.com>
2024-07-08 14:35:39 +0700

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,12 @@ import type { AppProps } from 'next/app';
 import React from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  if (typeof Component === 'function') {
+    return <Component {...pageProps} />;
+  }
+
+  // Handle cases where Component is not a function (if needed)
+  return null; // Or return default behavior
 }
 
 export default MyApp;
